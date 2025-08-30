6b790f1be1fac94702b894c82fa8449f1e87267a
Kohei Nishiwaki <128705560+hduehgw0@users.noreply.github.com>
2025-08-30 14:39:41 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,26 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    debugger
   end
 
   def new
     @user = User.new
-    debugger
   end
+
+  def create
+    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
