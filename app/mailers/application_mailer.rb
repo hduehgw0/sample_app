b0bc0580bc79c9917b4b66581a0d773db0310584
Kohei Nishiwaki <128705560+hduehgw0@users.noreply.github.com>
2025-09-15 09:16:46 +0000

    メール送信者のデフォルトアドレスを変更

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "user@realdomain.com"
+  default from: "noreply@sandbox07d16271cf6249fe883a63f76ad7af69.mailgun.org"
   layout "mailer"
 end
